946d7c65811327b728ffa7e7024594cb2331bc4f
Gillian Donley <gillian.donley@hope.edu>
2026-04-06 10:55:57 -0400

    Add workflow to automatically build and deploy the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "Ryan McFall's Tour of Heroes";
+  title = "Stu Dent's Tour of Heroes";
 }
